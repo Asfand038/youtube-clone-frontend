24fee4644c550e82b403111350d2b95dea5a4830
Asfandyar <jamali.asfandyar786@gmail.com>
2021-05-03 20:58:25 +0500

    checking for pre-commit hooks

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Youtube Clone</title>
-        <meta name='description' content="This is an awesome youtube clone" />
+        <meta name="description" content="This is an awesome youtube clone" />
         <link rel="icon" href="/favicon.svg" />
       </Head>
+      <h1>An awesome youtube clone</h1>
     </div>
-  )
+  );
 }
